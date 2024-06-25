1c90a0d25e0aec4ebcdae78c323e7d08c722fe88
Befarge <venikov2011@yandex.ru>
2024-06-26 00:01:05 +0300

    1

---
+++ src/Shape.h
@@ -0,0 +1,40 @@
+#include <vector>
+
+
+struct Point2D{
+    double x;
+    double y;
+
+    Point2D(double x, double y);   
+};
+
+struct Point3D{
+    double x;
+    double y;
+    double z;
+
+    Point3D(double x, double y, double z);   
+};
+
+
+class Shape2D{
+    private:
+        friend bool min_compare(Point2D, Point2D);
+        friend bool sort_compare(Point2D, Point2D, std::vector<Point2D>);
+    protected:
+        std::vector<Point2D> sort(std::vector<Point2D>);
+    public:
+        virtual double getP() = 0;
+        virtual double getS() = 0;
+        void show_point();
+        void setPoint(unsigned, Point2D);
+        Point2D getPoint(unsigned);
+        std::vector<Point2D> vertex;
+};
+
+class Polygon : public Shape2D{
+    public:
+        Polygon(std::vector<Point2D>);
+        double getP();
+        double getS();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+#include "Shape.h"
 #include <iostream>
 
-int main() {
-	int n = 0;
-	std::cout << "Hello!\n";
-}
 
+int main(){
+    Polygon obj({Point2D(0,0),Point2D(2,2), Point2D(5,0)});
+
+    obj.show_point();
+   
+
+}
