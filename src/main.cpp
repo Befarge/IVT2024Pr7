bae35bb652c27447b70ec4da0a49008ae8483208
Timofey Kirichenko <timofeykirichenko@gmail.com>
2024-06-26 01:43:21 +0300

    Размышления

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 
-int main(){
+/*int main(){
     //Polygon obj({Point2D(0,0),Point2D(2,2), Point2D(5,0)});
 
     //obj.show_point();
    
     std::cout << "Hello from main func\n";
-}
+}*/
